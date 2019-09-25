787563820f0511c0273878232639f83833d732a8
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-09-24 21:21:21 -0700

    Add timer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,80 @@
+import Countdown from "react-countdown-now"
 import React from "react"
-import { Link } from "gatsby"
 import moment from "moment"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import './index.css'
 
-const daysRemaining = () => {
-  const eventdate = moment("2019-11-24")
-  const todaysdate = moment()
+const labelStyle = {
+  fontSize: "2em",
+  fontWeight: "500",
+  verticalAlign: "bottom",
+}
 
-  return eventdate.diff(todaysdate, "days")
+const numberStyle = {
+  height: "60px",
+  fontSize: "6em",
+  fontWeight: "800",
+}
+
+const containerStyle = {
+  alignItems: "flex-end",
+  display: "flex",
+  height: "10em",
+  width: "auto",
+  margin: "8px",
+}
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    return <span>Should be sorta good by now.. Text him!</span>
+  } else {
+    return (
+      <div>
+        <div className="timer-container">
+          <div className="number-container">
+            <span className="number">
+              {days}
+            </span>
+            <span className="label">
+              D
+            </span>
+          </div>
+          <div className="number-container">
+            <span className="number">
+              {hours}
+            </span>
+            <span className="label">
+              H
+            </span>
+          </div>
+          <div className="number-container">
+            <span className="number">
+              {minutes}
+            </span>
+            <span className="label">
+              M
+            </span>
+          </div>
+          <div className="number-container">
+            <span className="number">
+              {seconds}
+            </span>
+            <span className="label">
+              S
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Maybe in {daysRemaining()} days...</h1>
+    <SEO title="Climb timer" />
+    <Countdown date={moment("2019-11-24").valueOf()} renderer={renderer} />
   </Layout>
 )
 
